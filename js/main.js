5fba200999a1de635b6f5c2e2ffc0399ae4e36b7
Lance <Lance@thelpstory.com>
2015-12-11 07:57:54 -0700

    git cleanup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 // add scripts
-
+// Write a function that takes a ride object and a child object as arguments (see example objects below).
+// Determine if the child is tall enough for the ride (the function should return true or false)
 console.log("sanity check!");
+
+function Child(height,name,age){
+  this.height = height;
+  this.name = name;
+  this.age = age;
+}
+
+var kid1 = new Child(3,'Julian',3);
+var kid2 = new Child(4,'Cero',4);
+
+function Ride(min,max){
+  this.minHeight = min;
+  this.maxHeight = max;
+}
+
+var ride1 = new Ride(3,7);
+
+var ride2 = new Ride(4,7);
